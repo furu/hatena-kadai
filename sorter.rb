3e93d4273bac38a47755ac674aabd59fc9da22e7
furu <ba09219@gmail.com>
2013-07-06 21:59:16 +0900

    Implement sort method.
    選択ソートによる実装

--- sorter.rb
+++ sorter.rb
@@ -8,6 +8,15 @@ class Sorter
   end
 
   def set_values(values)
-    @values = values
+    @values = values.clone
+  end
+
+  def sort
+    sorted = []
+    until @values.empty?
+      sorted << @values.min
+      @values.delete_at(@values.index(@values.min))
+    end
+    @values = sorted
   end
 end
